f10e21a6897f1f8312cd9a403656f13a336f609d
bioastroiner <68257484+bioastroiner@users.noreply.github.com>
2023-12-01 16:37:28 +0330

    + robust logging for recipemaps

--- src/main/java/mods/bio/gttweaker/recipe/CTRecipeMaps.java
+++ src/main/java/mods/bio/gttweaker/recipe/CTRecipeMaps.java
@@ -1,21 +1,49 @@
 package mods.bio.gttweaker.recipe;
 
 import gregapi.recipes.Recipe;
+import minetweaker.MineTweakerAPI;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
-@ZenClass("mods.gregtechmod.recipe.RecipeMaps")
+@ZenClass("mods.gregtech.recipe.RecipeMaps")
 public class CTRecipeMaps {
 
 	@ZenMethod
 	public static CTRecipeMap getRecipeMap(String name) {
+		if(!Recipe.RecipeMap.RECIPE_MAPS.containsKey(name)){
+			MineTweakerAPI.logError("RECIPEMAP " + name + " NOT FOUND!");
+			return null;
+		}
 		return new CTRecipeMap(Recipe.RecipeMap.RECIPE_MAPS.get(name));
+	}
+
+	@ZenMethod
+	public static List<CTRecipeMap> getRecipeMaps(){
+		List<CTRecipeMap> ctMaps = new ArrayList<>();
+		Recipe.RecipeMap.RECIPE_MAPS.forEach((name,map)->{
+			ctMaps.add(new CTRecipeMap(map));
+			MineTweakerAPI.logInfo(name +
+					"\n {Max Inputs: " + map.mInputItemsCount + "}" +
+					"\n {Min Inputs: " + map.mMinimalInputItems + "}" +
+					"\n {Max Fluid Inputs: " + map.mInputFluidCount + "}" +
+					"\n {Min Fluid Inputs: " + map.mMinimalInputFluids + "}" );
+			// TODO outputs
+		});
+		return ctMaps;
+	}
+
+	@ZenMethod
+	public static void logRecipeMaps(){
+		Recipe.RecipeMap.RECIPE_MAPS.forEach((name,map)->{
+			MineTweakerAPI.logInfo(name +
+					"\n {Max Inputs: " + map.mInputItemsCount + "}" +
+					"\n {Min Inputs: " + map.mMinimalInputItems + "}" +
+					"\n {Max Fluid Inputs: " + map.mInputFluidCount + "}" +
+					"\n {Min Fluid Inputs: " + map.mMinimalInputFluids + "}" );
+			// TODO outputs
+		});
 	}
 }
