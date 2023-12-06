d8e6dcf1c9bb271e715a01308103b11adda2b92d
bioastroiner <68257484+bioastroiner@users.noreply.github.com>
2023-12-06 07:09:43 +0330

    * make unify a getter

--- src/main/java/mods/bio/gttweaker/oredict/CTIOreDictExpansion.java
+++ src/main/java/mods/bio/gttweaker/oredict/CTIOreDictExpansion.java
@@ -2,17 +2,13 @@ package mods.bio.gttweaker.oredict;
 
 import gregapi.oredict.OreDictManager;
 import minetweaker.api.item.IItemStack;
-import minetweaker.api.minecraft.MineTweakerMC;
 import minetweaker.api.oredict.IOreDictEntry;
-import minetweaker.mc1710.oredict.MCOreDictEntry;
-import mods.bio.gttweaker.command.GTCommand;
 import stanhebben.zenscript.annotations.ZenExpansion;
 import stanhebben.zenscript.annotations.ZenGetter;
-import stanhebben.zenscript.annotations.ZenMethod;
 
 @ZenExpansion("minetweaker.oredict.IOreDictEntry")
 public class CTIOreDictExpansion {
-	@ZenMethod
+	@ZenGetter
 	public static IItemStack unify(IOreDictEntry oreDictEntry){
 		return CTUnifier.unifyItem(oreDictEntry);
 	}
