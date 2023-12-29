bbee11e4324534a918943b3c20431f434b9c73d2
bioastroiner <68257484+bioastroiner@users.noreply.github.com>
2023-12-29 15:18:26 +0330

    * Check IO

--- src/main/java/mods/bio/gttweaker/core/RecipeHelper.java
+++ src/main/java/mods/bio/gttweaker/core/RecipeHelper.java
@@ -4,10 +4,15 @@ import gregapi.recipes.Recipe;
 import minetweaker.MineTweakerAPI;
 import mods.bio.gttweaker.mods.gregtech.recipe.CTRecipe;
 import mods.bio.gttweaker.mods.gregtech.recipe.CTRecipeMap;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
 
 public class RecipeHelper {
 
@@ -31,5 +36,58 @@ public class RecipeHelper {
 	public static boolean addGTRecipe(@Nonnull Recipe mRecipe, @Nonnull Recipe.RecipeMap mRecipeMap, boolean checkForCollisions) {
 		// if NULL was returned then that recipe already existed, IF check for collision is TRUE
 		return mRecipeMap.addRecipe(mRecipe, checkForCollisions, mRecipe.mFakeRecipe, mRecipe.mHidden) != null;
+	}
+
+	public static boolean checkRecipeIO(Recipe.RecipeMap aRecipeMap, List<ItemStack> aInputs, List<ItemStack> aOutputs, List<FluidStack> aInputFluids, List<FluidStack> aOutputFluids) {
+		if(aInputs.size() > aRecipeMap.mInputItemsCount){
+			MineTweakerAPI.logError(
+					"INPUT ITEMS COUNT MUST BE =< THAN {" +  aRecipeMap.mInputItemsCount + "}"
+			);
+			return true;
+		}
+		if(aOutputs.size() > aRecipeMap.mOutputItemsCount){
+			MineTweakerAPI.logError(
+					"OUTPUT ITEMS COUNT MUST BE =< THAN {" +  aRecipeMap.mOutputItemsCount + "}"
+			);
+			return true;
+		}
+		if(aInputs.size() < aRecipeMap.mMinimalInputItems){
+			MineTweakerAPI.logError(
+					"INPUT ITEMS COUNT MUST BE > THAN {" +  aRecipeMap.mMinimalInputItems + "}"
+			);
+			return true;
+		}
+		if(aRecipeMap.mNeedsOutputs && (aOutputs.isEmpty() && aOutputFluids.isEmpty())){
+			MineTweakerAPI.logError("OUTPUT ITEMS MUST BE > 0");
+			return true;
+		}
+
+		if(aInputFluids.size() > aRecipeMap.mInputFluidCount){
+			MineTweakerAPI.logError(
+					"INPUT FLUID COUNT MUST BE =< THAN {" +  aRecipeMap.mInputItemsCount + "}"
+			);
+			return true;
+		}
+		if(aOutputFluids.size() > aRecipeMap.mOutputFluidCount){
+			MineTweakerAPI.logError(
+					"OUTPUT FLUID COUNT MUST BE =< THAN {" +  aRecipeMap.mOutputItemsCount + "}"
+			);
+			return true;
+		}
+		if(aInputFluids.size() < aRecipeMap.mMinimalInputFluids){
+			MineTweakerAPI.logError(
+					"INPUT FLUID COUNT MUST BE > THAN {" +  aRecipeMap.mMinimalInputItems + "}"
+			);
+			return true;
+		}
+		if(aRecipeMap.mNeedsOutputs && (aOutputs.isEmpty() && aOutputFluids.isEmpty())){
+			MineTweakerAPI.logError("OUTPUT (FLUID+ITEM) MUST BE > 0");
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean checkRecipeIO(Recipe.RecipeMap aRecipeMap,Recipe aRecipe){
+		return checkRecipeIO(aRecipeMap, asList(aRecipe.mInputs), asList(aRecipe.mOutputs), asList(aRecipe.mFluidInputs), asList(aRecipe.mFluidOutputs));
 	}
 }
